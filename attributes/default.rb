cb8cb308a3277cd7cc126307d8505446cba37d42
Mike 'sealabcore' Taylor <miketaylor@me.com>
2014-08-25 11:28:22 -0700

    add paper trail settings

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,8 @@
 default['remote_syslog2']['install_dir'] = '/usr/local/bin'
 default['remote_syslog2']['version']     = 'v0.13'
 default['remote_syslog2']['filename']    = 'remote_syslog_linux_amd64.tar.gz'
+
+default[:papertrail][:host] = 'logs.papertrailapp.com'
+default[:papertrail][:port] = '514'
+default[:papertrail][:logs] = []
+default[:papertrail][:exclude_patterns] = []
