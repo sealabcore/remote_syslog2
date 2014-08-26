c17d6afd20d072778d5c5552538ecc4b4fa43998
Mike 'sealabcore' Taylor <miketaylor@me.com>
2014-08-25 20:16:01 -0700

    allow the user to own deploy

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,4 @@
 service "remote_syslog" do
-  provider Chef::Provider::Service::Redhat
   action :nothing
   supports :status => true, :start => true, :stop => true, :restart => true
 end
@@ -8,60 +7,63 @@ src_filename = node['remote_syslog2']['filename']
 src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
 extract_path = "#{Chef::Config['file_cache_path']}/remote_syslog2"
 
-remote_file src_filepath do
-  source "https://github.com/papertrail/remote_syslog2/releases/download/#{node['remote_syslog2']['version']}/#{node['remote_syslog2']['filename']}"
-  owner 'root'
-  group 'root'
-  mode "0644"
-end
+node[:deploy].each do |application, deploy|
 
-bash 'extract and copy executable' do
-  cwd ::File.dirname(src_filepath)
-  code <<-EOH
-    mkdir -p #{extract_path}
-    tar xzf #{src_filename} -C #{extract_path}
-    mv #{extract_path}/remote_syslog/remote_syslog #{node['remote_syslog2']['install_dir']}
-    EOH
-  not_if { ::File.exists?(extract_path) }
-end
+  remote_file src_filepath do
+    source "https://github.com/papertrail/remote_syslog2/releases/download/#{node['remote_syslog2']['version']}/#{node['remote_syslog2']['filename']}"
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0644"
+  end
 
-file "#{node['remote_syslog2']['install_dir']}/remote_syslog" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :touch
-end
+  bash 'extract and copy executable' do
+    cwd ::File.dirname(src_filepath)
+    code <<-EOH
+      mkdir -p #{extract_path}
+      tar xzf #{src_filename} -C #{extract_path}
+      mv #{extract_path}/remote_syslog/remote_syslog #{node['remote_syslog2']['install_dir']}
+      EOH
+    not_if { ::File.exists?(extract_path) }
+  end
 
-file "/tmp/remote_syslog.log" do
-  owner "root"
-  group "root"
-  mode "0777"
-  action :touch
-end
+  file "#{node['remote_syslog2']['install_dir']}/remote_syslog" do
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0755"
+    action :touch
+  end
 
-file "/tmp/remote_syslog.pid" do
-  owner "root"
-  group "root"
-  mode "0777"
-  action :touch
-end
+  file "/tmp/remote_syslog.log" do
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0777"
+    action :touch
+  end
 
-template "/etc/log_files.yml" do
-  action :create
-  owner 'root'
-  group 'root'
-  mode  '0644'
-  source 'logs.yml.erb'
-end
+  file "/tmp/remote_syslog.pid" do
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0777"
+    action :touch
+  end
 
-cookbook_file '/etc/init.d/remote_syslog' do
-  action :create
-  owner 'root'
-  group 'root'
-  mode '0755'
-  source 'remote_syslog.init'
+  template "/etc/log_files.yml" do
+    action :create
+    owner deploy[:user]
+    group deploy[:group]
+    mode  '0644'
+    source 'logs.yml.erb'
+  end
 
-  notifies :reload, "service[remote_syslog]", :immediately
+  cookbook_file '/etc/init.d/remote_syslog' do
+    action :create
+    owner deploy[:user]
+    group deploy[:group]
+    mode '0755'
+    source 'remote_syslog.init'
+
+    notifies :reload, "service[remote_syslog]", :immediately
+  end
 end
 
 service "remote_syslog" do
