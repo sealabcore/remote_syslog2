774f7fcee127311a23d0c5991935b45b478cac5c
Mike 'sealabcore' Taylor <miketaylor@me.com>
2014-08-25 17:49:44 -0700

    owner root, group opsworks

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,8 @@ extract_path = "#{Chef::Config['file_cache_path']}/remote_syslog2"
 
 remote_file src_filepath do
   source "https://github.com/papertrail/remote_syslog2/releases/download/#{node['remote_syslog2']['version']}/#{node['remote_syslog2']['filename']}"
-  owner 'deploy'
-  group 'root'
+  owner 'root'
+  group 'opsworks'
   mode "0644"
 end
 
@@ -26,34 +26,38 @@ bash 'extract and copy executable' do
 end
 
 file "#{node['remote_syslog2']['install_dir']}/remote_syslog" do
-  owner "deploy"
-  group "root"
+  owner "root"
+  group 'opsworks'
   mode "0755"
   action :touch
 end
 
 template "/etc/log_files.yml" do
   action :create
-  owner 'deploy'
+  owner 'root'
+  group 'opsworks'
   mode  '0644'
   source 'logs.yml.erb'
 end
 
 file "/etc/remote_syslog.log" do
-  owner 'deploy'
+  owner 'root'
+  group 'opsworks'
   mode  '0644'
   action :touch
 end
 
 file "/var/run/remote_syslog.pid" do
-  owner 'deploy'
+  owner 'root'
+  group 'opsworks'
   mode  '0644'
   action :touch
 end
 
 cookbook_file '/etc/init.d/remote_syslog' do
   action :create
-  owner 'deploy'
+  owner 'root'
+  group 'opsworks'
   mode '0777'
   source 'remote_syslog.init'
   notifies :enable, "service[remote_syslog]"
