e97daa0577b35e70e3016c4cf1caa060c876132c
Mike Taylor <mike.taylor.4@gmail.com>
2016-04-15 12:34:04 -0700

    update remote syslog url

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@ extract_path = "#{Chef::Config['file_cache_path']}/remote_syslog2"
 node[:deploy].each do |application, deploy|
 
   remote_file src_filepath do
-    source "https://remote-syslogger-dl.s3.amazonaws.com/remote_syslog_linux_amd64.tar.gz"
+    source "https://github.com/papertrail/remote_syslog2/releases/download/v0.17/remote_syslog_linux_amd64.tar.gz"
     owner deploy[:user]
     group deploy[:group]
     mode "0644"
